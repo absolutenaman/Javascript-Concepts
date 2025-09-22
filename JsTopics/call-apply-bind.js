9a18374ae8d4668e3a32ae43af9818bc97251311
absolutenaman <absolute.naman@gmail.com>
2025-09-22 23:05:38 +0530

    Used call apply bind method in js

--- JsTopics/call-apply-bind.js
+++ JsTopics/call-apply-bind.js
@@ -1,41 +1,29 @@
 /*
-The bind() method creates a new function that, when called, has its this keyword set to the provided value. 
+The Bind() Method creates a new function and when that new function is called it set this keyword to the first argument
+which is passed to the bind method, and if any other sequences of arguments preceding the first argument are passed to the
+bind method then they are passed as an argument to the new function when the new function is called.
+The Call() Method calls the function directly and sets this to the first argument passed to the call
+method and if any other sequences of arguments preceding the first argument
+are passed to the call method then they are passed as an argument to the function.
+The Apply() Method calls the function directly and sets this to the first argument passed to the
+apply method and if any other arguments provided as an
+array are passed to the call method then they are passed as an argument to the function.
 */
 
-var pokemon = {
-    firstname: 'Pika',
-    lastname: 'Chu ',
-    getPokeName: function() {
-        var fullname = this.firstname + ' ' + this.lastname;
-        return fullname;
+var pokemon={
+    name:"Pikachu",
+    print:function(stmt){
+        console.log(this.name + (stmt || "is a good pokemon"))
     }
-};
+}
+var jigglyPuffFunc={
+    name:"jigglyPuff"
+}
+pokemon.print.call(jigglyPuffFunc," is a sleepy pokemon");
+pokemon.print.apply(jigglyPuffFunc,[" is a fat pokemon"])
+bindedPokemonMethod=pokemon.print.bind(jigglyPuffFunc," is a sleepy pokemon")
+bindedPokemonMethod()
+pokemon.print()
 
-var pokemonName = function() {
-    console.log(this.getPokeName() + 'I choose you!');
-};
 
-var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
 
-logPokemon(); // 'Pika Chu I choose you!'
-
-/*
-call() and apply() serve the exact same purpose. The only difference between how they work is that call() 
-expects all parameters to be passed in individually, whereas apply() expects an array of all of our parameters.
-*/
-
-var pokemon = {
-    firstname: 'Pika',
-    lastname: 'Chu ',
-    getPokeName: function() {
-        var fullname = this.firstname + ' ' + this.lastname;
-        return fullname;
-    }
-};
-
-var pokemonName = function(snack, hobby) {
-    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
-};
-
-pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
-pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
