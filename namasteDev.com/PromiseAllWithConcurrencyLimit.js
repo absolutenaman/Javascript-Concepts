7be414b1023f83a68986fe44ee88ba0eaa54e102
absolutenaman <absolute.naman@gmail.com>
2025-10-16 23:54:51 +0530

    Solved polyfill of all,allsettled and halfway finished through the promiseAllwithConcurrencyLimit

--- namasteDev.com/PromiseAllWithConcurrencyLimit.js
+++ namasteDev.com/PromiseAllWithConcurrencyLimit.js
@@ -1,52 +1,42 @@
-function promiseAllWithConcurrencyLimit(arr, limit) {
-    let curr = 0;
-    let result = [];
+const arr = [
+    new Promise((resolve) =>
+        setTimeout(() => resolve("Promise 1 resolved after 1 second"), 10000)),
+    new Promise((resolve) =>
+        setTimeout(() => resolve("Promise 2 resolved after 2 seconds"), 2000)
+    ),
+    new Promise((resolve) =>
+        setTimeout(() => resolve("Promise 3 resolved after 3 seconds"), 3000)
+    ),
+    new Promise((resolve) =>
+        setTimeout(() => resolve("Promise 4 resolved after 4 seconds"), 4000))
+];
+
+promiseAllWithConcurrencyLimit(arr, 2)
+    .then((r) => console.log("All results:", r))
+    .catch((err) => console.error("Error:", err));
+
+function promiseAllWithConcurrencyLimit(promisesArr, limit) {
     let index = 0;
-    let completed=0;
+    let currtasksInQueue = 0;
+    let ans = []
     return new Promise((resolve, reject) => {
         function next() {
-            if (completed === arr.length) {
-                resolve(result)
-                return;
-            }
-            while (curr < limit && index < arr.length) {
-                curr++;
-                let currIndex=index;
-                index++;
-                arr[currIndex]().then((res) => {
-                    result[currIndex] = res;
-                    completed++;
-                    curr--;
-                    next();
+            while (currtasksInQueue < limit && index < promisesArr.length) {
+                currtasksInQueue++;
+                promisesArr[index].then((res) => {
+                    ans.push(res)
+                    currtasksInQueue--;
+                    index++;
+                    console.log(res)
+                    next()
+                    if (ans.length === promisesArr.length)
+                        return resolve(ans)
                 }).catch((err) => {
-                    reject(err);
+                    reject(err)
                 })
             }
         }
-        next()
+        next();
     })
 }
 
-
-const promises = [
-    () =>
-        new Promise((resolve) =>
-            setTimeout(() => resolve("Promise 1 resolved after 1 second"), 1000)
-        ),
-    () =>
-        new Promise((resolve) =>
-            setTimeout(() => resolve("Promise 2 resolved after 2 seconds"), 2000)
-        ),
-    () =>
-        new Promise((resolve) =>
-            setTimeout(() => resolve("Promise 3 resolved after 3 seconds"), 3000)
-        ),
-    () =>
-        new Promise((resolve) =>
-            setTimeout(() => resolve("Promise 4 resolved after 4 seconds"), 4000)
-        ),
-];
-
-promiseAllWithConcurrencyLimit(promises, 2)
-    .then((r) => console.log("All results:", r))
-    .catch((err) => console.error("Error:", err));
