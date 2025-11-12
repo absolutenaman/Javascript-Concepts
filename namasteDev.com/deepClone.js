4adb8662059c362c47a2070bad70a708ecc2cbf9
absolutenaman <absolute.naman@gmail.com>
2025-11-12 23:52:10 +0530

    Feeling excited for future

--- namasteDev.com/deepClone.js
+++ namasteDev.com/deepClone.js
@@ -1,16 +1,21 @@
-const obj1 = { a: 1, b: { c: 2 }, d: [3, 4], c: undefined, d: null, e: "#$" };
+let obj1 = { a: 1, b: { c: 2 }, d: [3, 4], c: undefined, d: null, e: "#$" };
 function deepClone(obj) {
-  if (typeof obj !== "object") {
+    let result;
+    if(Array.isArray(obj))
+    result=[];
+  if (typeof obj !== "object" || Array.isArray(obj)) {
     return obj;
   } else {
-    if(!Array.isArray(obj)){
-        for(const key in obj){
-            if(typeof key==="object"){
-                obj[key]=deepClone(obj[key])
-            }
+      for (let i=0;i<obj.length;i++) {
+        if (Array.isArray(obj[key]))
+            
+        if (typeof key === "object") {
+          obj[key] = deepClone(obj[key]);
         }
-    }
-  }
+      }
+    } 
+   return obj;
 }
-console.log( deepClone(obj1));
-
+let result=(deepClone(obj1));
+obj1.b.c=4;
+console.log(result);
